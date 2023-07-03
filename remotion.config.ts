0ccc95f56b2dcb06b64d8f1ee44e2c1bf1768a71
Jonny Burger <jonathanburger11@gmail.com>
2023-07-03 11:56:56 +0200

    Revert "Discard changes to remotion.config.ts"
    This reverts commit 7e5b691c3d37345d16e3ef1eda5453e7fe7e5b28.

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,7 +1,9 @@
 // All configuration options: https://remotion.dev/docs/config
 // Each option also is available as a CLI flag: https://remotion.dev/docs/cli
-// ! The configuration file does only apply if you render via the CLI !
 
-import {Config} from 'remotion';
+// Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 
-Config.setImageFormat('jpeg');
+import {Config} from '@remotion/cli/config';
+
+Config.setChromiumOpenGlRenderer('angle');
+Config.setVideoImageFormat('jpeg');
